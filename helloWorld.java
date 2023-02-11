65ea715945c991f57af4e9533bca579714ab63fd
Mukesh-Negi <107361627+Mukesh-Negi@users.noreply.github.com>
2023-02-11 22:43:57 +0530

    First Program of Java
    printing hello world

---
+++ helloWorld.java
@@ -0,0 +1,21 @@
+import java.lang.System; //using library
+
+class Main {
+    public static void main(String arg[]) {
+        System.out.println("HELLO WORLD");
+    }
+}
+
+/*
+ * public :-> to access outside class
+ * static :-> to call without creating the object
+ */
+
+/*
+ * How to compile and run program in terminal.
+ * 1. Save the filename with extension ".java" --> eg. a.java
+ * 2. Open terminal and navigate to folder where file is saved. --> eg. cd
+ * Desktop
+ * 3. javac a.java --> a.java is filename
+ * 4. java Main ----> Main is the name of class
+ */
