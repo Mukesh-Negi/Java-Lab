9ce8c25e683b2a77496510513b9d40c2ce5a00b0
Mukesh-Negi <107361627+Mukesh-Negi@users.noreply.github.com>
2023-02-25 23:01:35 +0530

    compareTo( ) function For Strings

--- StringCompare.java
+++ StringCompare.java
@@ -1,6 +1,6 @@
+// wap to find which of two strings "Hello" or "hello" is larger.
 import java.lang.System;
 import java.util.Scanner;
-// wap to find which of two strings "Hello" or "hello" is larger.
 public class StringCompare
 {
 	public static void main(String[] args)
@@ -8,10 +8,11 @@ public class StringCompare
 		String s1 = "Hello";
 		String s2 = "hello";
 		String s3 = "bye";
-		/* compareTo() function is used to compare the strings. syntax:-> s1.compareTo(s2);
-		:-> it compares the first character of string.
-		:-> it returns +ve value if s1 is larger than s2
-		:-> it returns 0 if s1 and s2 both are equal
+		/*
+		compareTo() function is used to compare the strings.  syntax :-> s1.compareTo(s2);
+		:-> it compares the first character of strings.
+		:-> it returns +ve value if s1 is larger than s2.
+		:-> it returns 0 if s1 and s2 both are equal.
 		:-> it returns a -ve value if s1 is smaller than s2.
 		*/
 		System.out.println( s1.compareTo(s2) );
