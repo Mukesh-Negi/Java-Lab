929e7c58361a5d0719297a59f7aa43d507f22fd9
Mukesh-Negi <107361627+Mukesh-Negi@users.noreply.github.com>
2023-03-18 16:05:08 +0000

    merge two sorted arrays

--- mergeSorted.java
+++ mergeSorted.java
@@ -3,16 +3,51 @@
 import java.lang.System;
 import java.util.Arrays;
 import java.util.Scanner;
-class arrayInput
+class mergeSorted
 {
     public static void main(String[] args) {
+        int arr1[] = new int[5];
+        int arr2[] = new int[5];
         Scanner sc = new Scanner(System.in);
-        int arr[] = new int[10];
-        System.out.println("ENTER 10 ELEMENTS OF ARRAY :->");
-        for( int i = 0;  i < arr.length; i++)
+        System.out.println("ENTER 5 ELEMENTS OF ARRAY 1 IN SORTED ORDER :->");
+        for( int i = 0;  i < arr1.length; i++)
         {
-            arr[i] = sc.nextInt();
+            arr1[i] = sc.nextInt();
         }
-        System.out.println("ELEMENTS OF ARRAY :" + Arrays.toString(arr));
+        System.out.println("ENTER 5 ELEMENTS OF ARRAY 2 IN SORTED ORDER :->");
+        for(int i = 0; i < arr2.length; i++)
+        {
+            arr2[i] = sc.nextInt();
+        }
+        int merged[] = new int[arr1.length + arr2.length];
+        int i = 0; int j = 0;
+        int k = 0;
+        while( i < arr1.length && j < arr2.length)
+        {
+            if( arr1[i] <= arr2[j])
+            {
+                merged[k] = arr1[i];
+                i++;
+            }
+            else
+            {
+                merged[k] = arr2[j];
+                j++;
+            }
+            k++;
+        }
+        while( i < arr1.length)
+        {
+            merged[k] = arr1[i];
+            i++;
+            k++;
+        }
+        while( j < arr2.length)
+        {
+            merged[k] = arr2[j];
+            j++; 
+            k++;
+        }
+        System.out.println(" MERGED ARRAY :->" + Arrays.toString(merged));
     }
 }
