c05f6972b38b3ff55042bc030ffafa919ec6e2db
Mukesh-Negi <107361627+Mukesh-Negi@users.noreply.github.com>
2023-03-18 16:05:58 +0000

    merge two sorted arrays in java

--- mergeSorted.java
+++ mergeSorted.java
@@ -1,5 +1,4 @@
 // WAP to merge two sorted arrays in a new array.
-// wAP to find sum of all elements of array and find maximum in the array.
 import java.lang.System;
 import java.util.Arrays;
 import java.util.Scanner;
